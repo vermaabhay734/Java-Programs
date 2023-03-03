4bd3cbef5d16bc685b61fd54ad35dd1ea022d146
abhay734 <vermaabhay734@gmail.com>
2023-03-03 10:50:19 +0530

    Add Even or Odd

---
+++ Numbers/EvenOrOdd.java
@@ -0,0 +1,23 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args){
+        System.out.println("Enter a Number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num<=0){
+            System.out.println("Invalid number: "+num);
+        }
+        else{
+            if(num%2 == 0){
+                System.out.println(num+" is even");
+            }
+            else{
+                System.out.println(num+" is odd");
+            }
+        }
+        sc.close();
+    }
+}
