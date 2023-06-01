1487a5883cb3d2d6997da1ba2828b6ac88839b8a
abhay734 <vermaabhay734@gmail.com>
2023-06-01 11:10:36 +0530

    Pascal and inverted pascal Pattern

---
+++ Patterns/PascalTriangle.java
@@ -0,0 +1,37 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=row-1; i>=1; i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+/*
+Enter the number of rows: 5
+*
+**
+***
+****
+*****
+***
+**
+*
+ */
